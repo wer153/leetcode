223556979d2568dacb1a6c02c9f0d750da552495
Kim Minki <wer153@naver.com>
2023-08-24 19:44:25 +0900

    Time: 37 ms (79.18%), Space: 16.4 MB (29.66%) - LeetHub

---
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
@@ -0,0 +1,13 @@
+from functools import cache
+
+class Solution:
+    @cache
+    def tribonacci(self, n: int) -> int:
+        if n == 0: return 0
+        if n == 1: return 1
+        if n == 2: return 1
+        return (
+            self.tribonacci(n-3)
+            + self.tribonacci(n-2)
+            + self.tribonacci(n-1)
+        )
