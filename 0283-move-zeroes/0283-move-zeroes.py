306edfa2ce2b9982d9ddaa6a6497b78ffd667577
Kim Minki <wer153@naver.com>
2023-08-30 21:06:10 +0900

    Time: 142 ms (86.42%), Space: 17.9 MB (84.14%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,11 +3,11 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        count, index = 0, 0
-        while index < len(nums):
-            if nums[index] == 0:
-                del nums[index]
-                index -= 1
-                count += 1
-            index += 1
-        nums.extend([0] * count)
+        i, j = 0, len(nums) - 1
+        while i < j:
+            if nums[i] == 0:
+                del nums[i]
+                nums.append(0)
+                j -= 1
+            else:
+                i += 1
