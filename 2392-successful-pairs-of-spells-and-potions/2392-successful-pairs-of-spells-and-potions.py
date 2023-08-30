74355991a5220fb2b17225760e90b07cd6da49f4
Kim Minki <wer153@naver.com>
2023-08-30 20:20:16 +0900

    Time: 1010 ms (87.98%), Space: 39.6 MB (59.52%) - LeetHub

---
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.py
@@ -0,0 +1,10 @@
+from bisect import bisect_left
+
+class Solution:
+    def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+        def get_num_of_pair(spell):
+            return len(potions) - bisect_left(potions, success / spell)
+        potions.sort()
+        return [get_num_of_pair(spell) for spell in spells]
+
+
