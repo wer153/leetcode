cff0883a78cf2c47fba94220378d8d1d78c5752f
Kim Minki <wer153@naver.com>
2023-09-10 14:38:29 +0900

    Time: 36 ms (86.54%), Space: 16.2 MB (68.67%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,3 +1,3 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        nums[:] = [num for num in nums if num != val]
+        nums[:] = list(filter(lambda x: x!=val, nums))
