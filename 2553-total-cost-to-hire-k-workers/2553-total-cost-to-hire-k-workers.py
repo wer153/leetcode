03937a8232b6994caf7e79b643b4da43b6e1bcea
Kim Minki <wer153@naver.com>
2023-09-26 19:15:57 +0900

    Time: 1146 ms (5.38%), Space: 43 MB (5.03%) - LeetHub

--- 2553-total-cost-to-hire-k-workers/2553-total-cost-to-hire-k-workers.py
+++ 2553-total-cost-to-hire-k-workers/2553-total-cost-to-hire-k-workers.py
@@ -22,7 +22,7 @@ class Solution:
                 j -= 1
 
         popped_index = set()
-        front_heap, mid_heap, back_heap = [], [], []
+        front_heap, back_heap = [], []
         total_hiring_cost, i, j = 0, 0, len(costs) - 1
         for _ in range(candidates):
             heappush(front_heap, (costs[i], i))
@@ -30,52 +30,20 @@ class Solution:
             i += 1
             j -= 1
 
-        # print(costs, k, candidates, front_heap, back_heap)
-        # print(i, j, total_hiring_cost)
-        # print(front_heap, back_heap)
         for _ in range(k):
             front, back = heappop(front_heap), heappop(back_heap)
-            # print(front, front_heap)
-            # print(back, back_heap)
-            # total_hiring_cost += min([front[0], back[0]])
-            # popped_index.add(min([front,back]))
+            smaller = front if front <= back else back
+            popped_index.add(smaller)
+            total_hiring_cost += smaller[0]
             if front == back:
-                # print('equal')
-                popped_index.add(front)
                 push_front_heap()
                 push_back_heap()
-                total_hiring_cost += front[0]
             if front < back:
                 heappush(back_heap, back)
-                popped_index.add(front)
                 push_front_heap()
-                total_hiring_cost += front[0]
             if front > back:
                 heappush(front_heap, front)
-                popped_index.add(back)
                 push_back_heap()
-                total_hiring_cost += back[0]
-                
-            
-            # print(i, j, total_hiring_cost)
-            # if front_heap[0] == back_heap[0]:
-            #     total_hiring_cost += heappop(front_heap)[0]
-            #     if i < len(costs):
-            #         heappush(front_heap, (costs[i], i))
-            #     if 0 <= j:
-            #         heappush(back_heap, (costs[j], j))
-            #     i += 1
-            #     j -= 1
-            # elif front_heap[0] < back_heap[0]:
-            #     total_hiring_cost += heappop(front_heap)[0]
-            #     if i < len(costs):
-            #         heappush(front_heap, (costs[i], i))
-            #     i += 1
-            # elif front_heap[0] > back_heap[0]:
-            #     total_hiring_cost += heappop(back_heap)[0]
-            #     if 0 <= j:
-            #         heappush(back_heap, (costs[j], j))
-            #     j -= 1
                 
         return total_hiring_cost
 
