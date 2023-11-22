99c11e86184efb7d3298ffb3a07bafec1c2fb025
Kim Minki <wer153@naver.com>
2023-11-22 22:03:09 +0900

    Time: 80 ms (79.64%), Space: 19.5 MB (58.49%) - LeetHub

---
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -0,0 +1,19 @@
+from itertools import accumulate
+
+class Solution:
+    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        # solution1
+        # 1 m, mo, mou, mous, ... 와 같은 규칙으로 문자를 누적시키며 순회한다.
+        # 2 현재의 문자열로 시작하는 products만 필터링하고 사전순으로 정렬한다.
+        answers = []
+        products.sort()
+        for prefix in accumulate(searchWord):
+            if all(product.startswith(prefix) for product in products[:3]):
+                answers.append(products[:3])
+            else:
+                products = [product for product in products if product.startswith(prefix)]
+                answers.append(products[:3])
+        return answers
+                
+                
+        
