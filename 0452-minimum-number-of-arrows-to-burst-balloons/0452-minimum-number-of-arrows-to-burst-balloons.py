56893494540a84878712442cd3a072242449e67e
Kim Minki <wer153@naver.com>
2023-12-20 15:11:23 +0900

    Time: 1197 ms (30.54%), Space: 53.4 MB (99.11%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -4,10 +4,10 @@ from operator import itemgetter
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
         count = 0
-        dq = deque(sorted(points, key=itemgetter(1,0)))
-        while dq:
-            start, end = dq.popleft()
+        points.sort(key=itemgetter(1,0), reverse=True)
+        while points:
+            start, end = points.pop()
             count += 1
-            while dq and dq[0][0] <= end:
-                dq.popleft()
+            while points and points[-1][0] <= end:
+                points.pop()
         return count
