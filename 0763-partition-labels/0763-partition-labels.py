8efad1829034953506bc3501e126bbdac8f4021b
Kim Minki <wer153@naver.com>
2024-01-10 17:54:03 +0900

    Time: 133 ms (5.19%), Space: 17.4 MB (13.35%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        return [len(partition) for partition in generate_partitions(s)]
+
+def generate_partitions(string):
+    counter = Counter(string)
+    partition_list, partition_set = list(), set()
+    for char in string:
+        partition_list.append(char)
+        partition_set.add(char)
+        print(partition_list, partition_set)
+        counter[char] -= 1
+        if all(counter[element] == 0 for element in partition_set):
+            yield partition_list
+            partition_list, partition_set = list(), set()
+
+        
+    
