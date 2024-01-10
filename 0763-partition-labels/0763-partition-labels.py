c463324fe82d50d072ec16fef8c6f07bb69386c1
Kim Minki <wer153@naver.com>
2024-01-10 18:02:35 +0900

    Time: 51 ms (29.65%), Space: 17.5 MB (13.35%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -2,7 +2,7 @@ from collections import Counter
 
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        return [len(partition) for partition in generate_partitions(s)]
+        return [partition_length for partition_length in generate_partitions(s)]
 
 def generate_partitions(string):
     counter = Counter(string)
@@ -11,8 +11,8 @@ def generate_partitions(string):
         partition_set.add(char)
         counter[char] -= 1
         if all(counter[element] == 0 for element in partition_set):
-            yield string[start:index]
-            partition_set = set()
+            yield index-start
+            partition_set.clear()
             start = index
             
 
