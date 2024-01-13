4df3ed6810955d7a13a8da268beca534c6464f85
Kim Minki <wer153@naver.com>
2024-01-13 21:03:50 +0900

    Time: 1223 ms (7.72%), Space: 19.6 MB (5.27%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
@@ -5,34 +5,37 @@ class Solution:
     def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
         x_range, y_range = range(len(maze)), range(len(maze[0]))
         
-        def is_exit(x, y) -> bool:
-            return any([
+        def is_cell(x, y) -> bool:
+            if x in x_range and y in y_range:
+                if maze[x][y] == '.':
+                    return True
+            return False
+
+        def is_exit(node) -> bool:
+            x, y = node
+            return is_cell(x, y) and any([
                 x == x_range[0],
                 x == x_range[-1],
                 y == y_range[0],
                 y == y_range[-1],
             ])
-        
-        def get_neighbor_cells(x, y):
-            for x_prime, y_prime in ((x+1,y), (x-1,y), (x,y+1), (x,y-1)):
-                if x_prime in x_range and y_prime in y_range:
-                    if maze[x_prime][y_prime] == '.':
-                        yield (x_prime, y_prime)
 
-        visited = set()
-        batch = {tuple(entrance)}
-        step = 0
+        def get_next_batch(batch):
+            def get_neighbor_cells(x, y):
+                return [
+                    (x_prime, y_prime)
+                    for x_prime, y_prime in ((x+1,y), (x-1,y), (x,y+1), (x,y-1))
+                    if is_cell(x_prime, y_prime)
+                ]
+            next_batch = set()
+            for cell in batch:
+                for neighbor in get_neighbor_cells(*cell):
+                    if neighbor not in visited:
+                        next_batch.add(neighbor)
+            return next_batch
+
+        batch, visited, step = {tuple(entrance)}, set(), 0
         while batch:
-            if 0 < step and any([True for cell in batch if is_exit(*cell)]):
-                return step
-            step += 1
-            visited |= batch
-            batch = {
-                cell
-                for cell in set(chain.from_iterable(get_neighbor_cells(*cell) for cell in batch))
-                if cell not in visited
-            }
-        else:
-            return -1
-
-        return step
+            if 0 < step and any(filter(is_exit, batch)): return step
+            batch, visited, step = get_next_batch(batch), visited | batch, step + 1
+        return -1
