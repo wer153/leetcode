8b1fa68a9e23296ef643317695598a027b4adf53
Kim Minki <wer153@naver.com>
2024-01-14 17:05:23 +0900

    Time: 417 ms (50.56%), Space: 18.5 MB (37.06%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canJump(self, nums: list[int]) -> bool:
+        def jump_back(start):
+            for index, curr in enumerate(range(start-1, -1, -1), start=1):
+                if index <= nums[curr]:
+                    return start - index
+            return -1
+
+        index = len(nums)-1
+        while index != 0:
+            index = jump_back(index)
+            if index < 0: return False
+        return True
+            
