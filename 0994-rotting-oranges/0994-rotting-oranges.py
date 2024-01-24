2fe1acbac072d260910f39c4f04f9142c44c58ef
Kim Minki <wer153@naver.com>
2024-01-24 22:27:04 +0900

    Time: 60 ms (31.59%), Space: 16.6 MB (56.82%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -2,23 +2,19 @@ class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         def get_adjacent_oranges(rotten_oranges):
             adjacent_oranges = set()
-            for x,y in rotten_oranges:
+            for i,j in rotten_oranges:
                 for dx,dy in ((1,0),(-1,0),(0,1),(0,-1)):
-                    if (x+dx) in range(len(grid)) and (y+dy) in range(len(grid[0])):
-                        adjacent_oranges.add((x+dx,y+dy))
+                    x, y = i+dx, j+dy
+                    if x in range(len(grid)) and y in range(len(grid[0])):
+                        adjacent_oranges.add((x,y))
             return adjacent_oranges
 
         rotten_oranges = {
-            (i,j)
-            for i in range(len(grid))
-            for j in range(len(grid[0]))
+            (i,j) for i in range(len(grid)) for j in range(len(grid[0]))
             if grid[i][j] == 2
         }
-        
         fresh_oranges = {
-            (i,j)
-            for i in range(len(grid))
-            for j in range(len(grid[0]))
+            (i,j) for i in range(len(grid)) for j in range(len(grid[0]))
             if grid[i][j] == 1
         }
         batch_count = 0
